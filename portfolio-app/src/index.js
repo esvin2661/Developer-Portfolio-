591d3e1fde4e9a9f349d3e0411136599f1a996b2
esvin2661 <eruiz005@my.wilmu.edu>
2023-04-28 04:00:35 -0400

    Added TailWind CSS

--- portfolio-app/src/index.js
+++ portfolio-app/src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './index.css'
+import 'tailwindcss/dist/base.min.css'
+import 'tailwindcss/dist/components.min.css'
+import 'tailwindcss/dist/utilities.min.css'
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,6 +16,7 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
