383013df0b006be62972a590a2dd606255aef37b
esvin2661 <eruiz005@my.wilmu.edu>
2023-05-15 18:44:08 -0400

    builing home component

--- my-app/src/pages/components/nav.tsx
+++ my-app/src/pages/components/nav.tsx
@@ -1,1 +1,11 @@
-export {}
+import React from 'react';
+
+type MyComponentProps = {
+  name: string;
+};
+
+const MyComponent: React.FC<MyComponentProps> = ({ name }) => {
+  return <div>Hello, {name}!</div>;
+};
+
+export default MyComponent;
