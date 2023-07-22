c038f55a8ed1b2d95569064df69883b7481f9e60
esvin2661 <eruiz005@my.wilmu.edu>
2023-07-21 22:50:33 -0600

    Deleted starter content

--- esvin-portfolio/src/app/page.tsx
+++ esvin-portfolio/src/app/page.tsx
@@ -1,8 +1,10 @@
   import React from 'react';
   import 'bootstrap/dist/css/bootstrap.css'
 
-export default function Home() {
+export default function Page() {
   return <main>
     <h1>Test</h1>
   </main>;
 }
+
+
