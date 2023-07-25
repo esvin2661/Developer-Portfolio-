9e6f2ea042e762af370d242990546a7690fb5ab4
esvin2661 <eruiz005@my.wilmu.edu>
2023-07-24 18:23:01 -0600

    working on navbar

--- esvin-portfolio/src/app/layout.tsx
+++ esvin-portfolio/src/app/layout.tsx
@@ -1,19 +1,17 @@
-import { Navbar } from "./components/Navbar"
+import React from 'react';
+import Navbar from './components/Navbar';
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children} 
-        <div> 
-          <Navbar />
-          <h1>Layout</h1>
-        </div>
-      </body>
-    </html>
-  )
+    <div>
+      <Navbar />
+      <h1>Layout</h1>
+      {children}
+    </div>
+  );
 }
 
