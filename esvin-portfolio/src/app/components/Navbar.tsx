0c4650e44145ece970c4ad788fd40e4253100c07
esvin2661 <eruiz005@my.wilmu.edu>
2023-07-24 18:23:01 -0600

    working on navbar

--- esvin-portfolio/src/app/components/Navbar.tsx
+++ esvin-portfolio/src/app/components/Navbar.tsx
@@ -1,34 +1,35 @@
 import React from 'react'
+import Link from 'next/link';
 import Projects from '../projects/page';
+import About from '../about/page';
+import Contact from '../contact/page';
+import Photos from '../photos/page';
 
-export const Navbar = () => {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <a className="navbar-brand" href="#">Navbar</a>
-    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav">
-        <li className="nav-item active">
-          <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">About</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Projects</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link disabled" href="#">Photos</a>
-        </li>
-        <li className="nav-item">
-         <a className="nav-link" href="#">Contact</a>
-        </li>    
-      </ul>
-    </div>
-  </nav>
-  );
-};
 
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a className="navbar-brand" href="#">Navbar</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item active">
+                <Link href="/">
+                  <span className="nav-link">Home</span>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link href="/about">
+                  <span className="nav-link">About</span>
+                </Link>
+              </li>
+              {/* Add other navbar items here */}
+            </ul>
+          </div>
+        </nav>
+      );
+    };
+    
 export default Navbar;
