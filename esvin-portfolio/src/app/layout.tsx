ebe6d2d7b2b47affe987eed9c15495c790f2e1f1
esvin2661 <eruiz005@my.wilmu.edu>
2023-08-02 19:55:59 -0600

    Worked on components

--- esvin-portfolio/src/app/layout.tsx
+++ esvin-portfolio/src/app/layout.tsx
@@ -2,6 +2,10 @@
 import React from 'react';
 import { ChakraProvider } from '@chakra-ui/react'
 import NavBar from './components/Navbar';
+import HeroSection from './components/HeroSection';
+import Projects from './projects/page';
+import Photos from './photos/page';
+import Contact from './contact/page';
 import Footer from './components/Footer';
 
 
@@ -14,7 +18,12 @@ export default function RootLayout({
     <div>
       <ChakraProvider>
         <NavBar />
+        <HeroSection />
+        <Projects />
+        <Photos />
        { children }
+       <Contact />
+       <Footer />
       </ChakraProvider>
     </div>
   );
