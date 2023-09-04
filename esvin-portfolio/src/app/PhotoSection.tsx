807a59e5d613b10ddef9c754f1eb6b7982d175c5
esvin2661 <eruiz005@my.wilmu.edu>
2023-09-04 00:27:09 -0400

    Updates to photo and project sections.

--- esvin-portfolio/src/app/PhotoSection.tsx
+++ esvin-portfolio/src/app/PhotoSection.tsx
@@ -1,4 +1,3 @@
-'use client'
 import React, { useState } from 'react';
 import {
   Box,
@@ -12,37 +11,64 @@ import {
 } from '@chakra-ui/react';
 import { BsArrowUpRight, BsHeartFill, BsHeart } from 'react-icons/bs';
 
-export default function Projects() {
-  const [liked, setLiked] = useState(false);
+export default function Photos() {
+  const cardData = [
+    {
+      imageUrl:
+        'https://images.unsplash.com/photo-1542435503-956c469947f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=8',
+      heading: 'Esvin Photo 1',
+      text:
+        'In this post, we will give an overview of what is new in React 18, and what it means for the future.',
+    },
+    {
+      imageUrl:
+        'https://images.unsplash.com/photo-1542435503-956c469947f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=8',
+      heading: 'Esvin Photo  2',
+      text:
+        'Another project description goes here.',
+    },
+    {
+      imageUrl:
+        'https://images.unsplash.com/photo-1542435503-956c469947f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=8',
+      heading: 'Esvin Photo 3',
+      text:
+        'Yet another project description for the third card.',
+    },
+  ];
 
   return (
     <div>
-    <Center py={6}>
-      <HStack spacing={4}>
-        <Card liked={liked} setLiked={setLiked} imageUrl="https://images.unsplash.com/photo-1542435503-956c469947f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" />
-        <Card liked={liked} setLiked={setLiked} imageUrl="https://images.unsplash.com/photo-1542435503-956c469947f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" />
-        <Card liked={liked} setLiked={setLiked} imageUrl="https://images.unsplash.com/photo-1542435503-956c469947f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" />
-      </HStack>
-    </Center>
+      <Center py={6}>
+        <HStack spacing={4}>
+          {cardData.map((card, index) => (
+            <Card
+              key={index}
+              imageUrl={card.imageUrl}
+              heading={card.heading}
+              text={card.text}
+            />
+          ))}
+        </HStack>
+      </Center>
     </div>
   );
 }
 
-function Card({ liked, setLiked, imageUrl }: { liked: boolean; setLiked: React.Dispatch<React.SetStateAction<boolean>>; imageUrl: string }) {
+function Card({ imageUrl, heading, text }: { imageUrl: string; heading: string; text: string }) {
   return (
     <Box
-      w="xs"
-      rounded={'sm'}
-      my={5}
-      mx={[0, 5]}
-      overflow={'hidden'}
-      bg="white"
-      border={'1px'}
-      borderColor="black"
-      boxShadow={useColorModeValue('6px 6px 0 black', '6px 6px 0 cyan')}
-    >
+    w="xs"
+    rounded={'sm'}
+    my={5}
+    mx={[0, 5]}
+    overflow={'hidden'}
+    bg="white"
+    border={'1px'}
+    borderColor="black"
+    boxShadow={useColorModeValue('6px 6px 0 black', '6px 6px 0 cyan')}
+  >
       <Box h={'200px'} borderBottom={'1px'} borderColor="black">
-        <Img src={imageUrl} roundedTop={'sm'} objectFit="cover" h="full" w="full" alt={'Blog Image'} />
+        <Img src={imageUrl} roundedTop={'sm'} objectFit="cover" h="full" w="full" alt={'Card Image'} />
       </Box>
       <Box p={4}>
         <Box bg="black" display={'inline-block'} px={2} py={1} color="white" mb={2}>
@@ -51,10 +77,10 @@ function Card({ liked, setLiked, imageUrl }: { liked: boolean; setLiked: React.D
           </Text>
         </Box>
         <Heading color={'black'} fontSize={'2xl'} noOfLines={1}>
-          React v18.0
+          {heading}
         </Heading>
         <Text color={'gray.500'} noOfLines={2}>
-          In this post, we will give an overview of what is new in React 18, and what it means for the future.
+          {text}
         </Text>
       </Box>
       <HStack borderTop={'1px'} color="black">
@@ -71,9 +97,7 @@ function Card({ liked, setLiked, imageUrl }: { liked: boolean; setLiked: React.D
           roundedBottom={'sm'}
           borderLeft={'1px'}
           cursor="pointer"
-          onClick={() => setLiked(!liked)}
         >
-          {liked ? <BsHeartFill fill="red" fontSize={'24px'} /> : <BsHeart fontSize={'24px'} />}
         </Flex>
       </HStack>
     </Box>
