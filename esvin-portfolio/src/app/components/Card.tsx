b88b59d41ce7a335aa31298c0a6b70ea5b1d4e85
esvin2661 <eruiz005@my.wilmu.edu>
2023-09-06 00:32:32 -0400

    added night mode button to navbar

--- esvin-portfolio/src/app/components/Card.tsx
+++ esvin-portfolio/src/app/components/Card.tsx
@@ -1,81 +1,166 @@
-'use client'
-import React, { useState } from 'react';
+{/*  THIS IS MY ORIGINAL NAVBAR    SECTION */}
+import React from "react";
 import {
+  chakra,
   Box,
-  Heading,
-  Text,
-  Img,
   Flex,
-  Center,
-  useColorModeValue,
+  Avatar,
   HStack,
-} from '@chakra-ui/react';
-import { BsArrowUpRight, BsHeartFill, BsHeart } from 'react-icons/bs';
+  Text,
+  IconButton,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+  VisuallyHidden,
+  useColorMode, // Import useColorMode
+} from "@chakra-ui/react";
+import { HamburgerIcon, CloseIcon, MoonIcon, SunIcon } from "@chakra-ui/icons"; // Import MoonIcon and SunIcon
+import { FaGithub, FaYoutube, FaLinkedin } from "react-icons/fa";
+import { ReactNode } from 'react'
+import { useState } from "react";
 
-export default function Cards() {
-  const [liked, setLiked] = useState(false);
-
-  return (
-    <div>
-    <Center py={6}>
-      <HStack spacing={4}>
-        <Card liked={liked} setLiked={setLiked} imageUrl="https://images.unsplash.com/photo-1542435503-956c469947f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" />
-        <Card liked={liked} setLiked={setLiked} imageUrl="https://images.unsplash.com/photo-1542435503-956c469947f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" />
-        <Card liked={liked} setLiked={setLiked} imageUrl="https://images.unsplash.com/photo-1542435503-956c469947f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" />
-      </HStack>
-    </Center>
-    </div>
-  );
+interface NavLinkProps {
+  children: React.ReactNode;
+  href: string;
+  isExternal?: boolean;
 }
 
-function Card({ liked, setLiked, imageUrl }: { liked: boolean; setLiked: React.Dispatch<React.SetStateAction<boolean>>; imageUrl: string }) {
+const Links = [
+  { label: "Home", href: "/" },
+  { label: "Projects", href: "#project-section" },
+  { label: "Photos", href: "#photo-section" },
+  {
+    label: "Resume",
+    href: "https://blush-romola-69.tiiny.site/",
+    isExternal: true,
+  },
+];
+
+const SocialButton = ({
+  children,
+  label,
+  href,
+}: {
+  children: ReactNode
+  label: string
+  href: string
+}) => {
   return (
+    <chakra.button
+      bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
+      rounded={'full'}
+      w={8}
+      h={8}
+      cursor={'pointer'}
+      as={'a'}
+      href={href}
+      display={'inline-flex'}
+      alignItems={'center'}
+      justifyContent={'center'}
+      transition={'background 0.3s ease'}
+      _hover={{
+        bg: useColorModeValue('blackAlpha.200', 'whiteAlpha.200'),
+      }}>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      {children}
+    </chakra.button>
+  )
+}
+
+const NavLink = (props: NavLinkProps) => {
+  const { children, href, isExternal } = props;
+
+  const linkComponent = isExternal ? (
+    <a href={href} target="_blank" rel="noopener noreferrer">
+      {children}
+    </a>
+  ) : (
     <Box
-      w="xs"
-      rounded={'sm'}
-      my={5}
-      mx={[0, 5]}
-      overflow={'hidden'}
-      bg="white"
-      border={'1px'}
-      borderColor="black"
-      boxShadow={useColorModeValue('6px 6px 0 black', '6px 6px 0 cyan')}
+      as="a"
+      px={2}
+      py={1}
+      rounded={"md"}
+      _hover={{
+        textDecoration: "none",
+        bg: useColorModeValue("gray.200", "gray.700"),
+      }}
+      href={href}
     >
-      <Box h={'200px'} borderBottom={'1px'} borderColor="black">
-        <Img src={imageUrl} roundedTop={'sm'} objectFit="cover" h="full" w="full" alt={'Blog Image'} />
-      </Box>
-      <Box p={4}>
-        <Box bg="black" display={'inline-block'} px={2} py={1} color="white" mb={2}>
-          <Text fontSize={'xs'} fontWeight="medium">
-            React
-          </Text>
-        </Box>
-        <Heading color={'black'} fontSize={'2xl'} noOfLines={1}>
-          React v18.0
-        </Heading>
-        <Text color={'gray.500'} noOfLines={2}>
-          In this post, we will give an overview of what is new in React 18, and what it means for the future.
-        </Text>
-      </Box>
-      <HStack borderTop={'1px'} color="black">
-        <Flex p={4} alignItems="center" justifyContent={'space-between'} roundedBottom={'sm'} cursor={'pointer'} w="full">
-          <Text fontSize={'md'} fontWeight={'semibold'}>
-            View more
-          </Text>
-          <BsArrowUpRight />
-        </Flex>
-        <Flex
-          p={4}
-          alignItems="center"
-          justifyContent={'space-between'}
-          roundedBottom={'sm'}
-          borderLeft={'1px'}
-          cursor="pointer"
-          onClick={() => setLiked(!liked)}
-        >
-          {liked ? <BsHeartFill fill="red" fontSize={'24px'} /> : <BsHeart fontSize={'24px'} />}
-        </Flex>
-      </HStack>
+      {children}
     </Box>
+  );
+
+  return linkComponent;
+};
+
+export default function Simple() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { colorMode, toggleColorMode } = useColorMode(); // Get colorMode and toggleColorMode
+
+  return (
+    <>
+      <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
+        <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+          <IconButton
+            size={"md"}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label={"Open Menu"}
+            display={{ md: "none" }}
+            onClick={isOpen ? onClose : onOpen}
+          />
+          <HStack spacing={8} alignItems={"center"}>
+            <Box>Logo</Box>
+            <HStack
+              as={"nav"}
+              spacing={4}
+              display={{ base: "none", md: "flex" }}
+            >
+              {Links.map((link) => (
+                <NavLink
+                  key={link.href}
+                  href={link.href}
+                  isExternal={link.isExternal}
+                >
+                  {link.label}
+                </NavLink>
+              ))}
+            </HStack>
+          </HStack>
+          <Flex alignItems={"center"}>
+            <IconButton
+              icon={colorMode === "light" ? <MoonIcon /> : <SunIcon />} // Toggle button based on color mode
+              onClick={toggleColorMode} // Toggle color mode on button click
+              aria-label={colorMode === "light" ? "Switch to dark mode" : "Switch to light mode"} // Aria label based on color mode
+              variant="ghost"
+            />
+            <Menu>
+              {/* ... Your existing menu code ... */}
+            </Menu>
+          </Flex>
+        </Flex>
+
+        {isOpen ? (
+          <Box pb={4} display={{ md: "none" }}>
+            <Stack as={"nav"} spacing={4}>
+              {Links.map((link) => (
+                <NavLink
+                  key={link.href}
+                  href={link.href}
+                  isExternal={link.isExternal}
+                >
+                  {link.label}
+                </NavLink>
+              ))}
+            </Stack>
+          </Box>
+        ) : null}
+      </Box>
+    </>
   );
 }
