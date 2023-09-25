fe82e5ff724d9af7244f9906e2503e94c5a20dbc
esvin2661 <eruiz005@my.wilmu.edu>
2023-09-25 01:42:15 -0400

    Added GitHub contributino Chart.

---
+++ esvin-portfolio/src/app/components/GitHubContributions.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const GitHubContributions = () => {
+  return (
+    <img
+      src="http://ghchart.rshah.org/esvin2661"
+      alt="Esvin Ruiz 2023 Github chart"
+    />
+  );
+};
+export default GitHubContributions;
