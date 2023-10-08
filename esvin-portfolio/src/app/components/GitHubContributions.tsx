642c63be3291ea414d50852f7cbd6e1a1e497be8
esvin2661 <eruiz005@my.wilmu.edu>
2023-10-08 01:39:02 -0400

    updated UI on github contribution icon

--- esvin-portfolio/src/app/components/GitHubContributions.tsx
+++ esvin-portfolio/src/app/components/GitHubContributions.tsx
@@ -1,19 +1,33 @@
-'use-client'
+"use-client";
 import React from "react";
+import {
+  chakra,
+  Box,
+  Flex,
+  HStack,
+  Center,
+} from "@chakra-ui/react";
 
-// This section handles the github contributios api as a img src 
+// This section handles the github contributios api as a img src
 const GitHubContributions = () => {
   return (
-    <div className="github-contributions">
-      <div className="contributinos-header"> <h1>GitHub Contributions</h1>
-      <img
-        src="http://ghchart.rshah.org/esvin2661"
-        alt="Esvin Ruiz 2023 Github chart"
-      />
-      </div>
-    </div>
+    <>
+      <Box>
+        <Flex h={500} alignItems={"center"} justifyContent={"Center"}>
+          <div className="github-contributions">
+            <div className="contributinos-header">
+              <h1>GitHub Contributions</h1>
+              <img
+                src="http://ghchart.rshah.org/esvin2661"
+                alt="Esvin Ruiz 2023 Github chart"
+                style={{ width: "800px", height: "auto" }} 
+              />
+            </div>
+          </div>
+        </Flex>
+      </Box>
+    </>
   );
 };
 
 export default GitHubContributions;
-
