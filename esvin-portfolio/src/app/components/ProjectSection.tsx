c8cbc77f10ddc31b0d7bd4c547e644ade3ba482e
esvin2661 <eruiz005@my.wilmu.edu>
2023-12-03 03:48:20 -0500

    working on card images

--- esvin-portfolio/src/app/components/ProjectSection.tsx
+++ esvin-portfolio/src/app/components/ProjectSection.tsx
@@ -15,6 +15,7 @@ import {
 import { BsArrowUpRight } from 'react-icons/bs';
 
 const ProjectData = [
+  //ONLY UPLOAD PNG IMAGES 
   {
     imageUrl:
       'https://images.unsplash.com/photo-1542435503-956c469947f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=8',
