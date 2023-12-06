49d3f411c478d67bafe31446aab3c6ed9eab7b23
esvin2661 <eruiz005@my.wilmu.edu>
2023-12-06 05:15:24 -0500

    Fixed footer sectiona and removed portfolio sec

--- esvin-portfolio/src/app/components/Footer.tsx
+++ esvin-portfolio/src/app/components/Footer.tsx
@@ -9,6 +9,7 @@ import {
   useColorModeValue,
   VisuallyHidden,
 } from '@chakra-ui/react'
+import { Link } from '@chakra-ui/react';
 import { FaGithub, FaYoutube,FaLinkedin } from 'react-icons/fa'
 import { ReactNode } from 'react'
 
@@ -62,57 +63,60 @@ export default function Footer() {
     <Box>
       <Container
         as={Stack}
-        maxW={'6xl'}
+        maxW={"6xl"}
         py={4}
         spacing={4}
-        justify={'center'}
-        align={'center'}>
+        justify={"center"}
+        align={"center"}
+      >
         <Logo />
-        <Stack direction={'row'} spacing={6}>
-          <Box as="a" href={'/'}>
-            About 
-          </Box>
-          <Box as="a" href={'/projects'}>
-            Projects
-          </Box>
-          <Box as="a" href={'/photos'}>
-            Photos
-          </Box>
-          <Box as="a" href={'/portfolio'}>
-            Portfolio
-          </Box>
-          <Box as="a" href={'/Resume'}>
-            Resume
-          </Box>
+        <Stack direction={"row"} spacing={6}>
+          <Link href={"/"}>Home</Link>
+          <Link href={"#projects"}>Projects</Link>
+          <Link href={"#photos"}>Photos</Link>
+          {/* ADD Portfolio section later*/}
+          {/* <Link href={"/portfolio"}>Portfolio</Link> */}
+          <Link href={"/resume"}>Resume</Link>
         </Stack>
       </Container>
 
       <Box
         borderTopWidth={1}
-        borderStyle={'solid'}
-        borderColor={useColorModeValue('gray.200', 'gray.700')}>
+        borderStyle={"solid"}
+        borderColor={useColorModeValue("gray.200", "gray.700")}
+      >
         <Container
           as={Stack}
-          maxW={'6xl'}
+          maxW={"6xl"}
           py={4}
-          direction={{ base: 'column', md: 'row' }}
+          direction={{ base: "column", md: "row" }}
           spacing={4}
-          justify={{ base: 'center', md: 'space-between' }}
-          align={{ base: 'center', md: 'center' }}>
+          justify={{ base: "center", md: "space-between" }}
+          align={{ base: "center", md: "center" }}
+        >
           <Text>&copy; 2023 Esvin Ruiz. All rights reserved.</Text>
-          <Stack direction={'row'} spacing={6}>
-            <SocialButton label={'LinkedIn'} href={'https://www.linkedin.com/in/esv261/'}>
+          <Stack direction={"row"} spacing={6}>
+            <SocialButton
+              label={"LinkedIn"}
+              href={"https://www.linkedin.com/in/esv261/"}
+            >
               <FaLinkedin />
             </SocialButton>
-            <SocialButton label={'YouTube'} href={'https://www.youtube.com/channel/UCYyM0LuNo7ulbP7e6NcOxVw'}>
+            <SocialButton
+              label={"YouTube"}
+              href={"https://www.youtube.com/channel/UCYyM0LuNo7ulbP7e6NcOxVw"}
+            >
               <FaYoutube />
             </SocialButton>
-            <SocialButton label={'GitHub'} href={'https://github.com/esvin2661'}>
+            <SocialButton
+              label={"GitHub"}
+              href={"https://github.com/esvin2661"}
+            >
               <FaGithub />
             </SocialButton>
           </Stack>
         </Container>
       </Box>
     </Box>
-  )
+  );
 }
